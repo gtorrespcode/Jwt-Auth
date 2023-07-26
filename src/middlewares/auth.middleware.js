01deb5234097f1f4869fc9e8af43bac37d30d43d
Gabriel Torres <gabrieltorresp2009@gmail.com>
2023-07-26 04:26:04 -0300

    added the authMiddleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,42 @@
+import jwt from "jsonwebtoken";
+import { jwtSecret } from "../config/config.js";
+import userService from "../services/user.service.js";
+
+export const authMiddleware = (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      res.status(400).send({ message: "Not allowed" });
+    }
+
+    const parts = authorization.split(" ");
+
+    const [schema, token] = parts;
+
+    if (parts.length !== 2) {
+      res.status(400).send({ message: "Error in the authentication" });
+    }
+
+    if (schema !== "Bearer") {
+      res.status(400).send({ message: "Error in the authentication" });
+    }
+
+    jwt.verify(token, jwtSecret, async (err, decoded) => {
+      if (err) {
+        res.status(400).send({ message: "Invalid token" });
+      }
+
+      const user = await userService.findByIdService(decoded.id);
+
+      if (!user || !user.id) {
+        return res.status(400).send({ message: "Invalid token" });
+      }
+
+      req.userId = user.id;
+      return next();
+    });
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+};
